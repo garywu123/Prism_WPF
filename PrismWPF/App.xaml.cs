81b22376a18907003f3ad2102fbd51d8c28bb7fa
Gary Wu <gary.wu_1939@outlook.com>
2021-03-02 22:15:43 -0500

    创建一个初始应用

---
+++ PrismWPF/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace FirstApplication
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Window CreateShell()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
