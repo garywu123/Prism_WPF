862780ff92eda24cd2702c8e5e468af31209717c
Gary Wu <gary.wu_1939@outlook.com>
2021-03-03 15:59:16 -0500

    通过不同方法，将一个 Module 注册到 App 中。

--- ModuleA/ModuleAModule.cs
+++ ModuleA/ModuleAModule.cs
@@ -10,7 +10,6 @@
 ///     一个用来代表 View A 的模块。在 WPF PRISM 中，每一个 View 都是独立的模块。
 /// 该模块的采用的 WPF User Control Lib 模板，需要添加 Nuget Prism WPF。
 /// </summary>
-using System;
 using Prism.Ioc;
 using Prism.Modularity;
 
@@ -21,14 +20,21 @@ namespace ModuleA
     /// </summary>
     public class ModuleAModule : IModule
     {
+        /// <summary>
+        ///     当你在 App 中注册了该 Module，它会先实现该方法，将 <see cref="ModuleAModule" /> 注册到 App 的
+        ///     catalog map 中。
+        /// </summary>
+        /// <param name="containerRegistry"></param>
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            throw new NotImplementedException();
         }
 
+        /// <summary>
+        ///     当 Module 注册完成以后，调用该方法来初始化该 Module。
+        /// </summary>
+        /// <param name="containerProvider"></param>
         public void OnInitialized(IContainerProvider containerProvider)
         {
-            throw new NotImplementedException();
         }
     }
 }
--- PrismTemplateApp/App.xaml.cs
+++ PrismTemplateApp/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using ModuleA;
 using Prism.Ioc;
+using Prism.Modularity;
 using Prism.Regions;
 using PrismTemplateApp.Core.Regions;
 using PrismTemplateApp.Views;
@@ -32,5 +34,41 @@ namespace PrismTemplateApp
             regionAdapterMappings.RegisterMapping(typeof(StackPanel),
                 Container.Resolve<StackPanelRegionAdapter>());
         }
+
+        #region 注册 Modules
+
+        /// <summary>
+        ///     使用代码将 Module 注册到 Application 的 Catalog 中
+        /// </summary>
+        /// <param name="moduleCatalog"></param>
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+        
+            // 这一步，就是将 Module A 注册到 App 的 Catalog 中，这样 App 就能够使用该 Module。
+            moduleCatalog.AddModule<ModuleAModule>();
+        }
+
+        /* 使用其他方式注册
+         * /// <summary>
+        ///     通过直接指定 Module 的地址来注册 Module 到 Catalog 中。
+        /// </summary>
+        /// <returns> 返回一个 Module Catalog </returns>
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            // 注意，我们这里使用的路径是 PrismTemplateApp 项目下的 Modules 文件夹，但 Module 本身在另外一个项目中。
+            // 要能够过去到该 Module，需要在 ModuleA 的 BuildEvents 当中，创建 Build 脚本，使其在 Build 的时候，将对应的 dll
+            // 复制到 PrismTemplateApp 下的 Modules 文件夹
+            return new DirectoryModuleCatalog {ModulePath = @".\Modules"};
+        }
+
+        // 使用App.config 指定 Module 然后注册，注意，App.config 中定义的 Module 必须在 输出根目录中，
+        // 而不是再 Modules 文件夹中。
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }*/
+
+        #endregion
     }
 }
