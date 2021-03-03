4bfeaf773025d74890ba409bc565b8887e2cbfda
Gary Wu <gary.wu_1939@outlook.com>
2021-03-03 14:09:37 -0500

    创建自定义 RegionAdapter，定义 Region。
    RegionAdapter：就是将一个 View，转换为一个 Region

--- PrismTemplateApp/App.xaml.cs
+++ PrismTemplateApp/App.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using Prism.Ioc;
+using Prism.Regions;
+using PrismTemplateApp.Core.Regions;
 using PrismTemplateApp.Views;
 
 namespace PrismTemplateApp
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
@@ -16,7 +19,18 @@ namespace PrismTemplateApp
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+        }
 
+        /// <summary>
+        ///     该方法用来将你自定义的 <see cref="StackPanelRegionAdapter" /> 注册到系统表中，用来代表 StackPanel
+        /// </summary>
+        /// <param name="regionAdapterMappings"> Prism 中自带的控件与 Region 的映射表 </param>
+        protected override void ConfigureRegionAdapterMappings(
+            RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            regionAdapterMappings.RegisterMapping(typeof(StackPanel),
+                Container.Resolve<StackPanelRegionAdapter>());
         }
     }
 }
---
+++ PrismTemplateApp/Core/Regions/StackPanelRegionAdapter.cs
@@ -0,0 +1,82 @@
+﻿#region License
+
+// author:         吴经纬
+// created:        13:46
+// description:
+
+#endregion
+
+using System;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using Prism.Regions;
+
+namespace PrismTemplateApp.Core.Regions
+{
+    /// <summary>
+    ///     一个自定义的 StackPanelRegion，可以将一个普通的控件 StackPanel 转为一个 Region 从而可以被添加到 Prism 中的
+    ///     ContentControl 中
+    /// </summary>
+    public class StackPanelRegionAdapter : RegionAdapterBase<StackPanel>
+    {
+        /// <summary>
+        ///     必须实现的方法，用来创建 <see cref="RegionBehaviorFactory" />
+        /// </summary>
+        /// <param name="regionBehaviorFactory"> Region 行为工厂 </param>
+        public StackPanelRegionAdapter(IRegionBehaviorFactory regionBehaviorFactory) : base(
+            regionBehaviorFactory)
+        {
+        }
+
+        /// <summary>
+        ///     该方法用来将传入的 View 转为 StackPanel。每当你所定义的 Region 绑定的 Content
+        ///     列表发生变化，就会调用对应的事件，像你所定义的 StackPanel 中添加、删除对应的 target
+        /// </summary>
+        /// <param name="region">你在 xaml 中定义的 region</param>
+        /// <param name="regionTarget">
+        ///     目标控件，比如你想将 StackPanel 转为一个 Region，那你的 regionTarget
+        ///     就是 StackPanel
+        /// </param>
+        protected override void Adapt(IRegion region, StackPanel regionTarget)
+        { 
+            // 当region 的 Views 集合发生改变
+            region.Views.CollectionChanged += (s, e) =>
+            {
+                switch (e.Action)
+                {
+                    // 如果添加一个新的 View 到集合中
+                    case NotifyCollectionChangedAction.Add:
+                    {
+                        // 将所有控件添加到 StackPanel 中
+                        foreach (FrameworkElement item in e.NewItems)
+                            regionTarget.Children.Add(item);
+
+                        break;
+                    }
+                    case NotifyCollectionChangedAction.Remove:
+                    {
+                        foreach (FrameworkElement item in e.OldItems)
+                            regionTarget.Children.Remove(item);
+
+                        break;
+                    }
+                    case NotifyCollectionChangedAction.Replace:
+                        break;
+                    case NotifyCollectionChangedAction.Move:
+                        break;
+                    case NotifyCollectionChangedAction.Reset:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            };
+        }
+
+
+        protected override IRegion CreateRegion()
+        {
+            return new Region();
+        }
+    }
+}
