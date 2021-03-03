30bcd8a1fd90355cbcfa77fc320d58191cc8cc33
Gary Wu <gary.wu_1939@outlook.com>
2021-03-02 22:38:47 -0500

    - 2.1 什么是 Shell？
        Sehll 就是 root of your app... 它就是一个 Main application window，Master Page。而用来 hold 动态内容的地方称之为 region。
    
        - 2.1.1 创建一个 Shell
            1. 我们先和往常一样创建一个 ShellWindow 窗体，然后随便添加一些内容
            2. 在 App.xaml.cs 中，CreateShell 方法创建窗体
    
                ```java
                /// <summary>
                ///     创建一个 Shell 应用壳体
                /// </summary>
                /// <returns>返回一个窗口</returns>
                protected override Window CreateShell()
                {
                    // Container 是 Prism 中的一个属性，类似 IoC 依赖注入，将 ShellWindow 作为泛型传入以后，Container 会创建该 Window 然后返回
                    return Container.Resolve<ShellWindow>();
                }
                ```

--- PrismWPF/App.xaml.cs
+++ PrismWPF/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using FirstApplication.Views;
 using Prism.Ioc;
 using Prism.Unity;
 
@@ -8,16 +9,21 @@ namespace FirstApplication
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
-    public class App : PrismApplication
+    public partial class App : PrismApplication
     {
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            throw new NotImplementedException();
+            
         }
 
+        /// <summary>
+        ///     创建一个 Shell 应用壳体
+        /// </summary>
+        /// <returns>返回一个窗口</returns>
         protected override Window CreateShell()
         {
-            throw new NotImplementedException();
+            // Container 是 Prism 中的一个属性，类似 IoC 依赖注入，将 ShellWindow 作为泛型传入以后，Container 会创建该 Window 然后返回
+            return Container.Resolve<ShellWindow>();
         }
     }
 }
