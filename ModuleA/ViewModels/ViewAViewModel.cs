940ab0f2cd1a0c8583238b81abaebe2baee984d8
Gary Wu <gary.wu_1939@outlook.com>
2021-03-07 19:17:06 -0500

    # 使用 DelegateCommand
    使用委托 Command 的宗旨，就是当 WPF 调用一个空间的 Execute 的时候，其执行的将会是你定义的一个 委托，而不是 Execute 的 EventHandler。这样你可以将 Execute 需要实现的功能，写在你的 VM 中，然后 Execute 所做的就是触发该委托。
    
    ## 创建 DelegateCommand
    
    在 ViewA 中创建一个 Button，然后将其 Command 绑定到 ViewAViewModel 中的 ClickCommand 上。其实本质上它就是一个 `relaycommand`。
    
    ```java
    public DelegateCommand ClickCommand { get; }
    
    public ViewAViewModel()
    {
        ClickCommand = new DelegateCommand(Click, CanClick);
    }
    
    private bool CanClick()
    {
        return true;
    }
    
    private void Click()
    {
        Title = "View A ViewModel Title Changed by ClickMe Button";
    }
    ```
    # 触发 Change Notification
    
    假设我们有一个 Save 按钮，根据 application 的情况，它有时候会不可使用。那 WPF 是如何监控其是否可用呢？我们可以使用以下方式：
    
    - 当业务逻辑发生改变后，使用方法 SomeCommand.RaiseCanExecuteChanged 来通知 UI
    
        ```java
        private bool _isDirty = false;
    
        public bool IsDirty
        {
            get => _isDirty;
            set
            {
                SetProperty(ref _isDirty, value);
            // 通知 UI，SaveButton 的状态发生改变
                SaveCommand.RaiseCanExecuteChanged();
            }
        }
        ```
    
    - 在创建 DelegateCommand 的时候，添加 ObserverProperty 来监控某一个特定的 bool 属性
    
        ```java
        SaveCommand = new DelegateCommand(SaveExecutionMethod, CanSave)
                        .ObservesProperty(() => IsDirty);
        ```
    
    - 让 UI 监控 CanSave 方法的返回值
    
        ```java
        // 也可以用来监控 CanSave，通过这个，你不需要再实现 CanExecute 方法
        SaveCommand =
            new DelegateCommand(SaveExecutionMethod)
                .ObservesCanExecute(() => IsDirty);
        ```

--- ModuleA/ViewModels/ViewAViewModel.cs
+++ ModuleA/ViewModels/ViewAViewModel.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace ModuleA.ViewModels
@@ -8,8 +7,68 @@ namespace ModuleA.ViewModels
     /// <summary>
     ///     A View Model for ViewA
     /// </summary>
-    class ViewAViewModel : BindableBase
+    internal class ViewAViewModel : BindableBase
     {
-        public string Title { get; set; } = "Hello from ViewA ViewModel";
+        private string _title = "Hello from ViewA ViewModel";
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        /// <summary>
+        ///     True if the project has been changed and allowed to save the project
+        /// </summary>
+        private bool _isDirty = false;
+
+        public bool IsDirty
+        {
+            get => _isDirty;
+            set
+            {
+                // ReSharper disable once ArrangeAccessorOwnerBody
+                SetProperty(ref _isDirty, value);
+                // 通知 UI，SaveButton 的状态发生改变
+                // SaveCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public string OkButtonText { get; set; } = "Click Me";
+        public string SaveButtonText { get; set; } = "Save";
+
+        public DelegateCommand ClickCommand { get; }
+        public DelegateCommand SaveCommand { get; }
+
+        public ViewAViewModel()
+        {
+            ClickCommand = new DelegateCommand(ClickExecutionMethod, CanClick);
+
+            // 让 Save 按钮监视 IsDirty 属性
+            // SaveCommand = new DelegateCommand(SaveExecutionMethod, CanSave)
+            //     .ObservesProperty(() => IsDirty);
+
+            // 也可以用来监控 CanSave，通过这个，你不需要再实现 CanExecute 方法
+            SaveCommand =
+                new DelegateCommand(SaveExecutionMethod)
+                    .ObservesCanExecute(() => IsDirty);
+        }
+
+
+        private void SaveExecutionMethod()
+        {
+            MessageBox.Show("Project saved.");
+        }
+
+        private bool CanClick()
+        {
+            return true;
+        }
+
+        private void ClickExecutionMethod()
+        {
+            Title = "View A ViewModel Title Changed by ClickMe Button";
+            IsDirty = true;
+        }
     }
 }
